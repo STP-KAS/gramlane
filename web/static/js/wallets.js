a8bf5f2804ef231fec21542b857f136c7a3c3b03
STP-KAS <227352643+STP-KAS@users.noreply.github.com>
2026-09-03 22:12:36 +0200

    stop Kasware sendKaspa hang: pay from wallet UI, paste txid

--- web/static/js/wallets.js
+++ web/static/js/wallets.js
@@ -106,8 +106,6 @@
     if (payer) payer.value = c.address || "";
     const wallet = document.querySelector('input[name="wallet"]');
     if (wallet) wallet.value = c.id || "";
-    const pay = document.querySelector('input[name="payment"]');
-    if (pay && c.address && !pay.value) pay.value = (c.id ? c.id + ":" : "") + c.address;
   }
 
   async function connect(id) {
